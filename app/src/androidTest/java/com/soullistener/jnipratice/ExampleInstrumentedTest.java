19cf0b61840bb759e4a35ee52b65e3717ae519ff
kuan <1029671454@qq.com>
2019-01-23 10:34:26 +0800

    提取JNI工具类方便打jar包

--- app/src/androidTest/java/com/soullistener/jnipratice/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/soullistener/jnipratice/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.soullistener.jnipratice", appContext.getPackageName());
+        assertEquals("com.soullistener.com.soullistener.jnipratice", appContext.getPackageName());
     }
 }
